2ee5751e912277f125d234817ba0e1218e1dfefa
zadykian <zadykian@outlook.com>
2020-03-01 18:03:12 +0300

    get_table_metrics script

--- task1.1/get_tables_metrics.sh
+++ task1.1/get_tables_metrics.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Function to get size of table with name passed as argument '$1'.
-function getOriginalTableSizeInBytes()
+function getTableSizeInBytes()
 {
     if [ $# != 1 ];
     then
@@ -9,8 +9,9 @@ function getOriginalTableSizeInBytes()
         exit 1
     fi
 
-    local tableName=$1
-    #todo
+    local tableName=$1 
+    local commandText="select pg_relation_size(${table_name}::regclass);"
+    local tableSizeInBytes=$(psql -U postgres -c $commandText)
 }
 
 # Function to get size of TOAST table associated with table named as '$1'.
@@ -23,6 +24,20 @@ function getToastTableSizeInBytes()
     fi
 
     local originalTableName=$1
+    
+    #todo
+}
+
+# Function to measure execution time of select query to table with name '$1'.
+function measureSelectQueryTime()
+{
+    if [ $# != 1 ];
+    then
+        echo 'Invalid arguments count'
+        exit 1
+    fi
+
+    local tableName=$1;
     #todo
 }
 
@@ -31,6 +46,7 @@ schemaName='advanced_rds_task1'
 for toastStrategy in 'plain' 'extended' 'external' 'main';
 do
     tableName="${schemaName}.${toastStrategy}_table"
-    originalTableSize=$(getOriginalTableSizeInBytes $tableName)
+    originalTableSize=$(getTableSizeInBytes $tableName)
     toastTableSize=$(getToastTableSizeInBytes $tableName)
+    selectQueryTime=$(measureSelectQueryTime $tableName)
 done
