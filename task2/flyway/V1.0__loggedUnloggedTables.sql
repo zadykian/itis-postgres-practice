c401661a31a94a85604d05323576449c27c63f73
zadykian <zadykian@outlook.com>
2020-03-15 21:17:42 +0300

    flyway script

--- task2/flyway/V1.0__loggedUnloggedTables.sql
+++ task2/flyway/V1.0__loggedUnloggedTables.sql
@@ -1,3 +1,16 @@
 -- flyway script to create logged and unlogged
 -- tables with the same structure for benchmarking.
 
+create table public.logged_table
+(
+    id serial primary key,
+    text_column varchar(256),
+    date_column date
+)
+
+create table public.unlogged_table
+(
+    id serial primary key,
+    text_column varchar(256),
+    date_column date
+)
