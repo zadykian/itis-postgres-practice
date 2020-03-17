2534b3af3664236a6d21eb21bb1cb161976f4426
zadykian <zadykian@outlook.com>
2020-03-18 00:01:42 +0300

    fix

--- task2/pg_benchmarks/run_insert_benchmark.sh
+++ task2/pg_benchmarks/run_insert_benchmark.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-if [ $# != 1 ];
+if [ $# != 3 ];
 then
     echo 'Invalid arguments count'
     exit 1
@@ -9,9 +9,12 @@ fi
 pgbenchOptions="--host=advanced-rds-pg.csx2xlwomohu.us-east-1.rds.amazonaws.com "
 pgbenchOptions+="--port=5432 "
 pgbenchOptions+="--user=postgres "
-pgbenchOptions+="--time=5 "
 pgbenchOptions+="--define=table_name=$1 "
+pgbenchOptions+="--time=$2 "
+pgbenchOptions+="--client=$3 "
 pgbenchOptions+="--file=insert_benchmark.sql "
+pgbenchOptions+="--no-vacuum "
+
 pgbenchOptions+="homeworkdb"
 
 pgbench $pgbenchOptions
