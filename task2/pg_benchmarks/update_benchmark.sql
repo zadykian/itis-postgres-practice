cdf4d03d0a8750486b2a64e4471df8d633436d71
zadykian <zadykian@outlook.com>
2020-03-18 22:49:07 +0300

    *

--- task2/pg_benchmarks/update_benchmark.sql
+++ task2/pg_benchmarks/update_benchmark.sql
@@ -1,7 +1,7 @@
-\setshell idToUpdate ./get_id_to_update.sh :table_name
+\setshell id_to_update ./get_id_to_update.sh :table_name
 
 begin;
     update :table_name
     set text_column = null
-    where id = :idToUpdate;
+    where id = :id_to_update;
 end;
