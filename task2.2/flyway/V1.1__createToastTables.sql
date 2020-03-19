209735026865e402adaddb2f62342a2a83027f82
zadykian <zadykian@outlook.com>
2020-03-19 19:41:01 +0300

    flyway toast

---
+++ task2.2/flyway/V1.1__createToastTables.sql
@@ -0,0 +1,49 @@
+-- flyway script to create tables with different
+-- TOAST strategies for benchmarking.
+
+create schema task_2_2_toast;
+set search_path to task_2_2_toast;
+
+create table plain_strategy_table
+(
+    id serial primary key,
+    text_column varchar(4096),
+    date_column date
+);
+
+alter table plain_strategy_table
+alter column text column
+set storage plain;
+
+create table extended_strategy_table
+(
+    id serial primary key,
+    text_column varchar(4096),
+    date_column date
+);
+
+alter table plain_strategy_table
+alter column text column
+set storage extended;
+
+create table external_strategy_table
+(
+    id serial primary key,
+    text_column varchar(4096),
+    date_column date
+);
+
+alter table plain_strategy_table
+alter column text column
+set storage external;
+
+create table main_strategy_table
+(
+    id serial primary key,
+    text_column varchar(4096),
+    date_column date
+);
+
+alter table plain_strategy_table
+alter column text column
+set storage main;
