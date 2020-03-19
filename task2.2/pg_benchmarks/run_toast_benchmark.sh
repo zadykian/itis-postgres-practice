8d415f5e724679502a53a61a6f78efe65417f315
zadykian <zadykian@outlook.com>
2020-03-20 00:40:09 +0300

    *

--- task2.2/pg_benchmarks/run_toast_benchmark.sh
+++ task2.2/pg_benchmarks/run_toast_benchmark.sh
@@ -13,7 +13,7 @@ function generateRandomString()
 	cat /dev/urandom | tr -dc 'a-z0-9' | fold -w $randomStringLength | head -n 1
 }
 
-if [ $# != 1 ];
+if [ $# != 2 ];
 then
     echo 'Invalid arguments count'
     exit 1
@@ -24,8 +24,8 @@ randomString=$(generateRandomString 3072)
 options+="--host=advanced-rds-pg.csx2xlwomohu.us-east-1.rds.amazonaws.com "
 options+="--port=5432 "
 options+="--user=postgres "
-options+="--file=insert_benchmark.sql "
-options+="--time=$1 "
+options+="--file=$1 "
+options+="--time=$2 "
 options+="--no-vacuum "
 options+="--define=text_value='$randomString'"
 
