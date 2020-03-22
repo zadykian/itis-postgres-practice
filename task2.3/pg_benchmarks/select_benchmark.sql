a8c05063794c93e7e4ab6b8756eb27129db5be7d
zadykian <zadykian@outlook.com>
2020-03-22 20:26:21 +0300

    fix

--- task2.3/pg_benchmarks/select_benchmark.sql
+++ task2.3/pg_benchmarks/select_benchmark.sql
@@ -1,7 +1,9 @@
-\set id_to_select random(1, 10000)
+\set id_to_select random(2, 10001)
 
 begin;
     select *
     from :table_name
-    where id = :id_to_select;
+    where 
+        id = :id_to_select
+        and starts_with(text_column, 'h');
 end;
