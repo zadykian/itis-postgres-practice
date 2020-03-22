c60de66f445e1e0e244553059643acd9eaf28210
zadykian <zadykian@outlook.com>
2020-03-22 11:35:51 +0300

    fillfactor benchmarks

---
+++ task2.3/pg_benchmarks/insert_benchmark.sql
@@ -0,0 +1,4 @@
+begin;
+    insert into :table_name (number_column, text_column, date_column)
+    values (0, :text_value, '2020-03-22');
+end;
---
+++ task2.3/pg_benchmarks/run_fillfactor_benchmark.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+# Function to generate random string of length '$1'
+function generateRandomString()
+{
+	if [ $# != 1 ]; 
+	then
+		echo 'Invalid arguments count'
+		exit 1
+	fi
+
+	local randomStringLength=$1
+	cat /dev/urandom | tr -dc 'a-z0-9' | fold -w $randomStringLength | head -n 1
+}
+
+if [ $# != 2 ];
+then
+    echo 'Invalid arguments count'
+    exit 1
+fi
+
+randomString=$(generateRandomString 50)
+
+options+="--host=advanced-rds-pg.csx2xlwomohu.us-east-1.rds.amazonaws.com "
+options+="--port=5432 "
+options+="--user=postgres "
+options+="--no-vacuum "
+options+="--define=text_value='$randomString'"
+options+="--file=$1 "
+options+="$2 "
+
+schemaName="task_2_3_fillfactor"
+
+for fillfactorValue in 50 75 90 100;
+do
+	echo ""
+	echo "fillfactor: ${fillfactorValue}%:"
+
+    tableName="${schemaName}.table_${fillfactorValue}"
+    pgbench $options --define=table_name=$tableName "homeworkdb"
+done
