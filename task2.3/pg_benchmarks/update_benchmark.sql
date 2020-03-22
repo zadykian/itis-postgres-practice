06d85258ef723c1700d4bd68825834d12137f206
zadykian <zadykian@outlook.com>
2020-03-22 18:51:28 +0300

    fix

--- task2.3/pg_benchmarks/update_benchmark.sql
+++ task2.3/pg_benchmarks/update_benchmark.sql
@@ -1,9 +1,5 @@
-\set id_to_update random(2, 10001)
-
 begin;
     update :table_name
     set text_column = concat(text_column, text_column)
-    where
-        id = :id_to_update
-        and length(text_column) = 50;
+    where true;
 end;
