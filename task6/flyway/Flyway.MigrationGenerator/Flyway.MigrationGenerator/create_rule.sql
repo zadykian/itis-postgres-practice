be9fd902c36a2bc44532f33ae80d1e00cb6688d9
zadykian <zadykian@outlook.com>
2020-04-01 00:52:47 +0300

    fix

--- task6/flyway/Flyway.MigrationGenerator/Flyway.MigrationGenerator/create_rule.sql
+++ task6/flyway/Flyway.MigrationGenerator/Flyway.MigrationGenerator/create_rule.sql
@@ -2,6 +2,6 @@ create rule update_child_{0}_to_{1}
 as on update to child_{0}
       where old.id != new.id and new.id % 10 = {1}
 do instead (
-delete from task_6.child_{0} where id = old.id;
-insert into task_6.child_{1} values (new.*);
+    delete from task_6.child_{0} where id = old.id;
+    insert into task_6.child_{1} values (new.*);
 );
