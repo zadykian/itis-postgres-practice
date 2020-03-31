267d59ffd8ee4f8ef22259f6496eda15d5483973
zadykian <zadykian@outlook.com>
2020-03-31 21:10:33 +0300

    insert script

--- task6/flyway/V2.2__insert_into_hub_table.sql
+++ task6/flyway/V2.2__insert_into_hub_table.sql
@@ -1,6 +1,5 @@
 -- flyway script to load 1_000_000 rows into hub table.
 
-create sequence hub_table_id_seq as integer;
-
 insert into hub_table(id, name)
-values 
+select generate_series, 'name_value'
+from generate_series(1, 1000000);
