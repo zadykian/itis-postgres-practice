e0785f448b522e18c25642b463ea96b1bf5e4e50
zadykian <zadykian@outlook.com>
2020-03-31 23:55:01 +0300

    fix

--- task6/flyway/Flyway.MigrationGenerator/Flyway.MigrationGenerator/Program.cs
+++ task6/flyway/Flyway.MigrationGenerator/Flyway.MigrationGenerator/Program.cs
@@ -6,6 +6,13 @@ namespace Flyway.MigrationGenerator
 {
 	internal static class Program
 	{
+		private const string ruleText = @"
+			create rule update_child_{0}_to_{1}
+			as on update to child_{0}
+			where old.id != new.id and new.id % 10 = {1}
+			do instead
+			insert into task_6.child_{1} values (new.*);";
+		
 		private static void Main(string[] args)
 		{
 			if (args.Length != 1)
@@ -13,13 +20,6 @@ namespace Flyway.MigrationGenerator
 				Console.WriteLine("Invalid arguments count!");
 				Environment.Exit(-1);
 			}
-			
-			var ruleText = @"
-				create rule update_child_{0}
-				as on update to child_{0}
-				where old.id != new.id and id % 10 = {1}
-				do instead
-				insert into task_6.child_{1} values (new.*);";
 
 			using var fileStream = new FileStream(args[0], FileMode.Append);
 			using var streamWriter = new StreamWriter(fileStream);
@@ -30,12 +30,13 @@ namespace Flyway.MigrationGenerator
 					.Range(0, 9)
 					.Where(number => number != childTableNumber))
 				{
-					var ruleString = string.Format(ruleText, childTableNumber, destinationTableNumber);
+					var ruleString = string
+						.Format(ruleText, childTableNumber, destinationTableNumber)
+						.Replace("\t", string.Empty);
+					
 					streamWriter.WriteLine(ruleString);
 				}
 			}
-			
-			streamWriter.Flush();
 		}
 	}
 }
