7538621d878e39906f4d4c26c9a4d3dfcdedcaa4
zadykian <zadykian@outlook.com>
2020-03-31 23:20:20 +0300

    Flyway migrator project

---
+++ task6/flyway/Flyway.MigrationGenerator/Flyway.MigrationGenerator/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Flyway.MigrationGenerator
+{
+	internal static class Program
+	{
+		private static void Main(string[] args)
+		{
+			var ruleText = $@"
+				create rule update_child_0
+				as on update to child_0
+				where old.id != new.id and id % 10 = 1
+				do instead
+				insert into task_6.child_1 values (new.*);";
+		}
+	}
+}
--- task6/flyway/V2.1__create_insert_rules.sql
+++ task6/flyway/V2.1__create_insert_rules.sql
@@ -7,58 +7,58 @@ create rule insert_to_child_0
 as on insert to hub_table
 where new.id % 10 = 0
 do instead
-insert into child_0 values (new.*);
+insert into task_6.child_0 values (new.*);
 
 create rule insert_to_child_1
 as on insert to hub_table
 where new.id % 10 = 1
 do instead
-insert into child_1 values (new.*);
+insert into task_6.child_1 values (new.*);
 
 create rule insert_to_child_2
 as on insert to hub_table
 where new.id % 10 = 2
 do instead
-insert into child_2 values (new.*);
+insert into task_6.child_2 values (new.*);
 
 create rule insert_to_child_3
 as on insert to hub_table
 where new.id % 10 = 3
 do instead
-insert into child_3 values (new.*);
+insert into task_6.child_3 values (new.*);
 
 create rule insert_to_child_4
 as on insert to hub_table
 where new.id % 10 = 4
 do instead
-insert into child_4 values (new.*);
+insert into task_6.child_4 values (new.*);
 
 create rule insert_to_child_5
 as on insert to hub_table
 where new.id % 10 = 5
 do instead
-insert into child_5 values (new.*);
+insert into task_6.child_5 values (new.*);
 
 create rule insert_to_child_6
 as on insert to hub_table
 where new.id % 10 = 6
 do instead
-insert into child_6 values (new.*);
+insert into task_6.child_6 values (new.*);
 
 create rule insert_to_child_7
 as on insert to hub_table
 where new.id % 10 = 7
 do instead
-insert into child_7 values (new.*);
+insert into task_6.child_7 values (new.*);
 
 create rule insert_to_child_8
 as on insert to hub_table
 where new.id % 10 = 8
 do instead
-insert into child_8 values (new.*);
+insert into task_6.child_8 values (new.*);
 
 create rule insert_to_child_9
 as on insert to hub_table
 where new.id % 10 = 9
 do instead
-insert into child_9 values (new.*);
+insert into task_6.child_9 values (new.*);
