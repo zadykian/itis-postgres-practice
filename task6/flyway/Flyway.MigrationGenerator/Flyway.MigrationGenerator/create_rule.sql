ddf16dfb6e7ac029c461d94da29f89d41863b820
zadykian <zadykian@outlook.com>
2020-04-01 18:58:13 +0300

    fix

--- task6/flyway/Flyway.MigrationGenerator/Flyway.MigrationGenerator/create_rule.sql
+++ task6/flyway/Flyway.MigrationGenerator/Flyway.MigrationGenerator/create_rule.sql
@@ -1,13 +1,10 @@
-create rule move_from_{0}_to_{1}
-as on update to task_6.hub_table
+create or replace rule move_from_{0}_to_{1} as
+on update to task_6.hub_table
 where
 	old.id % 10 = {0}
 	and new.id % 10 = {1}
 do instead
 (
-	delete from task_6.child_{0}
-	where id = old.id;
-
-	insert into task_6.child_{1}
-	values (new.*);
+	insert into task_6.child_{1} values (new.*);
+	delete from task_6.child_{0} where id = old.id;
 );
