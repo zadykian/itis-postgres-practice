b5724fa5d14ea803956e4691d69338cf0770665e
zadykian <zadykian@outlook.com>
2020-04-04 19:21:58 +0300

    task7 pgbench

---
+++ task7/pg_benchmarks/run_benchmark.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+if [ $# != 1 ];
+then
+    echo 'Invalid arguments count'
+    exit 1
+fi
+
+options+="--host=advanced-rds-pg.csx2xlwomohu.us-east-1.rds.amazonaws.com "
+options+="--port=5432 "
+options+="--user=postgres "
+options+="--file=select_benchmark.sql "
+options+="--time=$1 "
+options+="--no-vacuum "
+
+pgbench $options "homeworkdb"
---
+++ task7/pg_benchmarks/select_benchmark.sql
@@ -0,0 +1,5 @@
+begin;
+    select id, name
+    from task_7.vacuum_test
+    where id = 0;
+end;
