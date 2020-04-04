ba8a1c362e6acac93a8e24e7c446591ac66853c2
zadykian <zadykian@outlook.com>
2020-04-04 14:28:10 +0300

    *

--- task6/pg_benchmarks/run_benchmark.sh
+++ task6/pg_benchmarks/run_benchmark.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-if [ $# != 0 ];
+if [ $# != 1 ];
 then
     echo 'Invalid arguments count'
     exit 1
@@ -10,7 +10,8 @@ options+="--host=advanced-rds-pg.csx2xlwomohu.us-east-1.rds.amazonaws.com "
 options+="--port=5432 "
 options+="--user=postgres "
 options+="--file=update_hub_table.sql "
-options+="--transactions=1000000 "
+options+="--transactions=$1 "
 options+="--no-vacuum "
+options+="--progress=60"
 
 pgbench $options "homeworkdb"
