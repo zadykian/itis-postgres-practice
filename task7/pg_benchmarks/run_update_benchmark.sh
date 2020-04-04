4b7a9f4036aef343655fc2462a411b7f731e1b73
zadykian <zadykian@outlook.com>
2020-04-04 20:31:05 +0300

    update

--- task7/pg_benchmarks/run_update_benchmark.sh
+++ task7/pg_benchmarks/run_update_benchmark.sh
@@ -9,8 +9,10 @@ fi
 options+="--host=advanced-rds-pg.csx2xlwomohu.us-east-1.rds.amazonaws.com "
 options+="--port=5432 "
 options+="--user=postgres "
-options+="--file=select_benchmark.sql "
-options+="--time=$1 "
+options+="--file=update_single_row.sql "
 options+="--no-vacuum "
+options+="--client=32 "
+options+="--jobs=4 "
+options+="--transactions=$1"
 
 pgbench $options "homeworkdb"
---
+++ task7/pg_benchmarks/update_single_row.sql
@@ -0,0 +1,6 @@
+\set random_value random(1, 1000000)
+
+begin;
+    update task_7.vacuum_test
+    set name = concat(left(name, 9), ' ', ':random_value')
+end;
