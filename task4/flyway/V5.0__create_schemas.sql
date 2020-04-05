36ec8941a3d65ec5dc06fac6852f36f7f4b2554a
zadykian <zadykian@outlook.com>
2020-04-05 21:35:49 +0300

    task4 migrations

---
+++ task4/flyway/V5.0__create_schemas.sql
@@ -0,0 +1,11 @@
+drop schema if exists task_4 cascade;
+create schema task_5;
+
+drop schema if exists task_4_foreign_0 cascade;
+create schema task_5_foreign_0;
+
+drop schema if exists task_4_foreign_1 cascade;
+create schema task_5_foreign_1;
+
+drop schema if exists task_4_foreign_2 cascade;
+create schema task_5_foreign_2;
---
+++ task4/flyway/V5.1__create_tables.sql
@@ -0,0 +1,17 @@
+create table task_4_foreign_0.entity_table
+(
+    id serial primary key,
+    name varchar(128) not null;
+);
+
+create table task_4_foreign_1.entity_table
+(
+    id serial primary key,
+    name varchar(128) not null;
+);
+
+create table task_4_foreign_2.entity_table
+(
+    id serial primary key,
+    name varchar(128) not null;
+);
