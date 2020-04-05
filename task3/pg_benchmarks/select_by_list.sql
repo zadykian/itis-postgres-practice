ab7b89f4aae121c025c75a70b41c02d9ad01cdcd
zadykian <zadykian@outlook.com>
2020-04-05 15:59:20 +0300

    scripts

--- task3/pg_benchmarks/select_by_list.sql
+++ task3/pg_benchmarks/select_by_list.sql
@@ -1,5 +1,26 @@
 begin;
     select id, name
     from task_3.table_to_cluster
-    where id in ();
+    where id in 
+    (
+        51395,
+        62792,
+        26166,
+        15757,
+
+        89922,
+        83133,
+        2299
+        76654,
+
+        279,
+        9250,
+        68311,
+        4771,
+
+        21510,
+        79268,
+        938,
+        74516
+    );
 end;
