424cb272fd35307bb2d945c8c21caa3196dbb85a
zadykian <zadykian@outlook.com>
2020-04-20 10:50:58 +0300

    *

--- task13/flyway/V10.2__instead_of_create_trigger.sql
+++ task13/flyway/V10.2__instead_of_create_trigger.sql
@@ -3,12 +3,12 @@ create function task_13.insert_into_view()
     language plpgsql as
 $func$
     begin
-        insert into task_13.underlying_table (typename, value)
-        values (new.typename, new.value);
+        insert into task_13.underlying_table (type_name, value)
+        values (new.*);
     end
 $func$;
 
 create trigger perform_insert
-instead of insert to task_13.group_by_view
+instead of insert on task_13.group_by_view
 for each row
 execute function task_13.insert_into_view();
