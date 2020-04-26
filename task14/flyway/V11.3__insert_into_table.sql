a6b54bf69987f6965385acd85a92c0c5c104a77a
zadykian <zadykian@outlook.com>
2020-04-26 22:21:51 +0300

    *

---
+++ task14/flyway/V11.3__insert_into_table.sql
@@ -0,0 +1,5 @@
+insert into task_14.table_with_complex_number (varchar_column, complex_number_column)
+select
+    concat('varchar_value_', int_value),
+    row(int_value, int_value + (random() * 10)::int)::complex_number
+from generate_series(1, 1000) as int_value;
