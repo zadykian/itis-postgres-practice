ce5fedf42abaa88aa29dfbe69966ba39b9951238
zadykian <zadykian@outlook.com>
2020-04-26 22:57:08 +0300

    *

--- task14/flyway/V11.3__insert_into_table.sql
+++ task14/flyway/V11.3__insert_into_table.sql
@@ -3,5 +3,5 @@ set search_path to task_14;
 insert into task_14.table_with_complex_number (varchar_column, complex_number_column)
 select
     concat('varchar_value_', int_value),
-    row(random(), random())::complex_number
+    row(round(random()::numeric, 2), round(random()::numeric, 2))::complex_number
 from generate_series(1, 1000) as int_value;
