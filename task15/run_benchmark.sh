79766b5c2e4f8db9cdbfd3bdd78f23276e9a1b0b
zadykian <zadykian@outlook.com>
2020-05-01 23:18:40 +0300

    *

--- task15/run_benchmark.sh
+++ task15/run_benchmark.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
 
+if [ $# != 1 ];
+then
+    echo 'Invalid arguments count'
+    exit 1
+fi
+
 options+="--host=54.175.156.101 "
 options+="--port=5432 "
-options+="--user=postgres "
+options+="--user=zadykian "
 options+="--progress=60 "
 
 options+="--client=10 "
 options+="--time=300 "
 
-pgbench $options "postgres"
+pgbench $options "dbname=postgres sslmode=$1 sslrootcert=root.crt sslcert=postgresql.crt sslkey=postgresql.key"
