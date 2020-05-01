45158b45f612a95ee8e1e4a5d9c1ea9b7ec19d88
zadykian <zadykian@outlook.com>
2020-05-01 22:38:24 +0300

    *

--- task15/run_benchmark.sh
+++ task15/run_benchmark.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-options+="--host=localhost "
+options+="--host=54.175.156.101 "
 options+="--port=5432 "
 options+="--user=postgres "
 options+="--progress=60 "
 
-options+="--client=100 "
+options+="--client=10 "
 options+="--time=300 "
 
 pgbench $options "postgres"
