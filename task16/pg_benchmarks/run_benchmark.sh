4e7a245e7951c0ae24a3eab9192e87c3bfd1b887
zadykian <zadykian@outlook.com>
2020-05-02 17:17:04 +0300

    *

--- task16/pg_benchmarks/run_benchmark.sh
+++ task16/pg_benchmarks/run_benchmark.sh
@@ -3,8 +3,11 @@
 options+="--host=advanced-rds-pg.csx2xlwomohu.us-east-1.rds.amazonaws.com "
 options+="--port=5432 "
 options+="--user=postgres "
+
 options+="--file=select_benchmark.sql "
+options+="--no-vacuum "
+
 options+="--time=300 "
-options+="--progress=60 "
+options+="--progress=30 "
 
 pgbench $options "homeworkdb"
