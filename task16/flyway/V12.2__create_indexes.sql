d4f889943fd84720250d4073636a7f5fb03fa1e5
zadykian <zadykian@outlook.com>
2020-05-02 16:25:54 +0300

    *

--- task16/flyway/V12.2__create_indexes.sql
+++ task16/flyway/V12.2__create_indexes.sql
@@ -1,1 +1,19 @@
 set search_path to task_16;
+
+create index on btree_test_table (A);
+create index on btree_test_table (A, B);
+create index on btree_test_table (A, B, C);
+create index on btree_test_table (A, C);
+create index on btree_test_table (A, C, B);
+
+create index on btree_test_table (B);
+create index on btree_test_table (B, A);
+create index on btree_test_table (B, A, C);
+create index on btree_test_table (B, C);
+create index on btree_test_table (B, C, A);
+
+create index on btree_test_table (C);
+create index on btree_test_table (C, A);
+create index on btree_test_table (C, A, B);
+create index on btree_test_table (C, B);
+create index on btree_test_table (C, B, A);
