7a89a361dd62079220cac24eddcac25b6dd800a5
zadykian <zadykian@outlook.com>
2020-05-03 19:58:44 +0300

    *

--- task19/flyway/V15.2__insert_into_tables.sql
+++ task19/flyway/V15.2__insert_into_tables.sql
@@ -3,17 +3,17 @@ set search_path to task_19;
 insert into btree_test_table (id, content)
 select 
     seq_value, 
-    public.get_random_string(256)
+    public.get_random_string(128)
 from generate_series(1, 100000) as seq_value;
 
 insert into gin_test_table (id, content)
 select 
     seq_value, 
-    public.get_random_string(256)
+    public.get_random_string(128)
 from generate_series(1, 100000) as seq_value;
 
 insert into gist_test_table (id, content)
 select 
     seq_value, 
-    public.get_random_string(256)
+    public.get_random_string(128)
 from generate_series(1, 100000) as seq_value;
