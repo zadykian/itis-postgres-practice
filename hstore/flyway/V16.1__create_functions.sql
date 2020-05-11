c69400356ca643a7243f6df65a90671a71f4d74b
zadykian <zadykian@outlook.com>
2020-05-11 13:16:29 +0300

    *

--- hstore/flyway/V16.1__create_functions.sql
+++ hstore/flyway/V16.1__create_functions.sql
@@ -1,13 +1,13 @@
 set search_path to hstore_benchmarks;
 
-create function generate_hstore_value(pairs_count int)
+create function generate_hstore_value(start_index int, pairs_count int)
     returns hstore
     immutable
     language plpgsql as
 $func$
     declare key_value_pairs text[];
     begin
-        for index in 1..pairs_count
+        for index in start_index..(start_index + pairs_count)
         loop
             key_value_pairs = array_append(key_value_pairs, format('key_%s => value_%s', index, index));
         end loop;
@@ -16,14 +16,14 @@ $func$
     end
 $func$;
 
-create function generate_json_value(pairs_count int)
+create function generate_json_value(start_index int, pairs_count int)
     returns json
     immutable
     language plpgsql as
 $func$
     declare key_value_pairs text[];
     begin
-        for index in 1..pairs_count
+        for index in start_index..(start_index + pairs_count)
         loop
             key_value_pairs = array_append(key_value_pairs, format('"key_%s": "value_%s"', index, index));
         end loop;
@@ -32,18 +32,46 @@ $func$
     end
 $func$;
 
-create function generate_jsonb_value(pairs_count int)
-    returns jsonb
-    immutable
+create function insert_into_test_tables(pairs_count int)
+    returns void
     language plpgsql as
 $func$
-    declare key_value_pairs text[];
     begin
-        for index in 1..pairs_count
-        loop
-            key_value_pairs = array_append(key_value_pairs, format('"key_%s": "value_%s"', index, index));
-        end loop;
+        set search_path to hstore_benchmarks;
 
-        return ('{' || array_to_string(key_value_pairs, ', ') || '}')::jsonb;
-    end
+        truncate table hstore_test_table;
+        insert into hstore_test_table(attributes)
+        select generate_hstore_value(seq_value, pairs_count)
+        from generate_series(1, 100000) as seq_value;
+
+        truncate table hstore_test_table_gin;
+        insert into hstore_test_table_gin(attributes)
+        select generate_hstore_value(seq_value, pairs_count)
+        from generate_series(1, 100000) as seq_value;
+
+        truncate table hstore_test_table_gist;
+        insert into hstore_test_table_gist(attributes)
+        select generate_hstore_value(seq_value, pairs_count)
+        from generate_series(1, 100000) as seq_value;
+
+        truncate table json_test_table;
+        insert into json_test_table(attributes)
+        select generate_json_value(seq_value, pairs_count)
+        from generate_series(1, 100000) as seq_value;
+
+        truncate table jsonb_test_table;
+        insert into jsonb_test_table(attributes)
+        select generate_json_value(pairs_count)
+        from generate_series(1, 100000) as seq_value;
+
+        truncate table jsonb_test_table_gin;
+        insert into jsonb_test_table_gin(attributes)
+        select generate_json_value(seq_value, pairs_count)
+        from generate_series(1, 100000) as seq_value;
+
+        truncate table jsonb_test_table_gin_path_ops;
+        insert into jsonb_test_table_gin_path_ops(attributes)
+        select generate_json_value(seq_value, pairs_count)
+        from generate_series(1, 100000) as seq_value;
+    end;
 $func$;
