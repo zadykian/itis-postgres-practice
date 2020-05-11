95f28c5cd8bf3a7146fd606fb4af83e3a3e2814b
zadykian <zadykian@outlook.com>
2020-05-11 14:31:37 +0300

    *

---
+++ hstore/pg_benchmarks/select_benchmark.sql
@@ -0,0 +1,5 @@
+begin;
+    select *
+    from hstore_benchmarks.:table_name
+    where attributes ?| '{key_16, key_32, key_64, key_128, key_256, key_512, key_1024, key_2048, key_4096, key_8192}';
+end;
