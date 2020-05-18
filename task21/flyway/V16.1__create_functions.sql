f32bcd47c93438fde7add5f8da6d7b0268eb823f
a.zadykyan <a.zadykyan@bars-open.ru>
2020-05-18 10:28:52 +0300

    task_21 functions

---
+++ task21/flyway/V16.1__create_functions.sql
@@ -0,0 +1,32 @@
+create or replace function task_21.sql_function(double precision left_bound)
+    returns setof task_21.functions_test_table
+    language sql as
+$func$
+    begin
+        select
+            id,
+            varchar_column,
+            double_column
+        from
+            task_21.functions_test_table
+        where
+            double_column >= left_bound;
+    end;
+$func$;
+
+create or replace function task_21.plpgsql_function(double precision left_bound)
+    returns setof task_21.functions_test_table
+    language plpgsql as
+$func$
+    begin
+        return query 
+        select
+            id,
+            varchar_column,
+            double_column
+        from
+            task_21.functions_test_table
+        where
+            double_column >= left_bound;
+    end;
+$func$;
