cb57402eca436a4f77eee6c754b79a7516819ba0
a.zadykyan <a.zadykyan@bars-open.ru>
2020-05-25 10:15:38 +0300

    *

--- task22/flyway/V18.1__shared_buffers_insert_data.sql
+++ task22/flyway/V18.1__shared_buffers_insert_data.sql
@@ -1,17 +1,17 @@
 insert into task_22.small_table (varchar_column, random_number)
 select
-    seq_value,
+    concat('varchar_value_', seq_value),
     random()
 from generate_series(1, 1000) as seq_value;
 
 insert into task_22.medium_table (varchar_column, random_number)
 select
-    seq_value,
+    concat('varchar_value_', seq_value),
     random()
 from generate_series(1, 10000) as seq_value;
 
 insert into task_22.large_table (varchar_column, random_number)
 select
-    seq_value,
+    concat('varchar_value_', seq_value),
     random()
 from generate_series(1, 100000) as seq_value;
