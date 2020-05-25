687c9517bd79312dae7c558fd501025c96684857
a.zadykyan <a.zadykyan@bars-open.ru>
2020-05-25 15:32:07 +0300

    *

--- task22/flyway/V18.1__shared_buffers_insert_data.sql
+++ task22/flyway/V18.1__shared_buffers_insert_data.sql
@@ -15,3 +15,15 @@ select
     concat('varchar_value_', seq_value),
     random()
 from generate_series(1, 100000) as seq_value;
+
+create or replace function task_22.select_all_data()
+returns setof task_22.small_table
+language sql as
+$func$
+    select *
+    from task_22.small_table
+    union all
+    select * from task_22.medium_table
+    union all
+    select * from task_22.large_table;
+$func$;
