512ef033c60c22ce574db85c00472a8b62252acc
a.zadykyan <a.zadykyan@bars-open.ru>
2020-05-25 10:34:34 +0300

    *

--- task22/flyway/V18.0__shared_buffers_create_tables.sql
+++ task22/flyway/V18.0__shared_buffers_create_tables.sql
@@ -4,21 +4,21 @@ set search_path to task_22;
 
 create table small_table
 (
-    id int primary key,
+    id serial primary key,
     varchar_column varchar(512) not null,
     random_number double precision not null
 );
 
 create table medium_table
 (
-    id int primary key,
+    id serial primary key,
     varchar_column varchar(512) not null,
     random_number double precision not null
 );
 
 create table large_table
 (
-    id int primary key,
+    id serial primary key,
     varchar_column varchar(512) not null,
     random_number double precision not null
 );
